2ccffd11f7752da60658d610856970132e81ffa2
cosmmiike <mikemiller@MacBook-Pro-Mike.local>
2019-03-18 16:22:08 +0100

    MVC v0.4 - Keyboard navigation

--- set_labeling/labeling/views.py
+++ set_labeling/labeling/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 import os
 import json
 
@@ -21,31 +22,35 @@ def labeling_view(request):
     with open(dataset_dir + 'dataset.json') as json_file:
         data = json.load(json_file)
 
-    if request.POST.get('photo-url') and\
-       request.POST.get('frisure-radio') and\
-       request.POST.get('couleur-radio') and\
-       request.POST.get('longeur-radio') and\
-       request.POST.get('volume-radio'):
-        output = {
-            'url': request.POST.get('photo-url'),
-            'frisure': request.POST.get('frisure-radio'),
-            'couleur': request.POST.get('couleur-radio'),
-            'longeur': request.POST.get('longeur-radio'),
-            'volume': request.POST.get('volume-radio'),
-        }
+    if request.POST:
+        if request.POST.get('photo-url') and\
+           request.POST.get('frisure-radio') and\
+           request.POST.get('couleur-radio') and\
+           request.POST.get('longeur-radio') and\
+           request.POST.get('volume-radio'):
+            output = {
+                'url': request.POST.get('photo-url'),
+                'frisure': request.POST.get('frisure-radio'),
+                'couleur': request.POST.get('couleur-radio'),
+                'longeur': request.POST.get('longeur-radio'),
+                'volume': request.POST.get('volume-radio'),
+            }
 
-        img_data = next((item for item in data['data'] if item['url'] == output['url']), None)
-        if img_data:
-            data['data'][:] = [d for d in data['data'] if d.get('url') != img_data['url']]
+            img_data = next((item for item in data['data'] if item['url'] == output['url']), None)
+            if img_data:
+                data['data'][:] = [d for d in data['data'] if d.get('url') != img_data['url']]
 
-        data['data'].append(output)
-        with open(dataset_dir + 'dataset.json', 'w') as outfile:
-            json.dump(data, outfile, indent=4)
+            data['data'].append(output)
+            with open(dataset_dir + 'dataset.json', 'w') as outfile:
+                json.dump(data, outfile, indent=4)
 
     img_num = 0
     next_img = request.GET.get('id_photo')
     if next_img and int(next_img) < len(photos) - 1:
-        img_num = int(next_img)
+        if int(next_img) < 0:
+            img_num = 0
+        else:
+            img_num = int(next_img)
 
     context = {
         'sets': sets,
